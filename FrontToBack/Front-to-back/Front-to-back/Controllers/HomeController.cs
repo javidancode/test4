419618cc5dc46529a7b29e5bdf36e6308c37a23a
javidancode <javidanig@code.edu.az>
2022-10-06 02:05:32 +0400

    Added Slider Database

---
+++ FrontToBack/Front-to-back/Front-to-back/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Front_to_back.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace Front_to_back.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Car car = new Car
+            {
+                Id = 1,
+                Name = "BMW"
+            };
+            return View(car);
+        }
+    }
+}
---
+++ FrontToBack/Front-to-back/Front-to-back/Models/Car.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Front_to_back.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
